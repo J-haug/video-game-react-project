8215220a0535e6ba99d28958a37059ee3ed66ae2
J-haug <jeremiahhaugan@gmail.com>
2025-12-30 16:16:07 -0600

    adding css to nav

--- src/components/SearchNav.jsx
+++ src/components/SearchNav.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import image from "../assets/cosmic-environment-with-millions-colorful-bright-lights.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function SearchNav() {
   return (
@@ -9,8 +11,9 @@ function SearchNav() {
           <img src={image} alt="" className="nav__background--img" />
         </figure>
         <div className="row">
-          <h1 className="search__title">Find the right game for you</h1>
+          <h1 className="search__title">Browse from our +10,000 games</h1>
           <div className="nav__search-bar">
+            <FontAwesomeIcon className="search__icon" icon={faMagnifyingGlass} />
             <input type="text" placeholder="Search" className="search__input" />
           </div>
         </div>
