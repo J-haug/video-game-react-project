083d59058e9739a4185b08d5f88d4218bb9573b0
J-haug <jeremiahhaugan@gmail.com>
2026-01-14 12:55:52 -0600

    made routes working

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 import SearchNav from './components/SearchNav';
 import Item from './components/Item';
 import Items from './components/Items';
+import Search from './pages/Search';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 
 
 function App() {
   return (
-    <div className="App">
-      <SearchNav />
-      <Items />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Search />} />
+        </Routes>
+        
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/pages/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchNav from '../components/SearchNav'
+import Items from '../components/Items'
+
+function Search() {
+  return (
+    <>
+    <SearchNav />
+    <Items />
+    
+    </>
+  )
+}
+
+export default Search
