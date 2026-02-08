062c8ad12013a632be7f5da65968e5f6a459f210
J-haug <jeremiahhaugan@gmail.com>
2026-02-07 21:58:00 -0600

    3

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -36,22 +36,33 @@ function ItemPage() {
   return (
     <>
       <Nav />
-      <div className="container">
-        <div className="row">
-          <div className="item__page">
-            <figure className="item__page__img--wrapper">
-              <img src={item.image_url} alt="" className="item__page--img" />
-            </figure>
-            <div className="item__description--wrapper">
-              <h1 className="item__page--title">{item.title}</h1>
-              <p className="item__page--description">{item.summary}</p>
-              <a href={item.url} className="item__page--link">
-                link
-              </a>
-            </div>
-          </div>
+       {isLoading ? (<> 
+       <div className="placeholder__container">
+        <div className="placeholder__row">
+          <div className="item-page__img--placeholder"></div>
+          <div className="item-page__title--placeholder"></div>
+
         </div>
-      </div>
+       </div>
+       </> ) : (
+         <div className="container">
+           <div className="row">
+             <div className="item__page">
+               <figure className="item__page__img--wrapper">
+                 <img src={item.image_url} alt="" className="item__page--img" />
+               </figure>
+               <div className="item__description--wrapper">
+                 <h1 className="item__page--title">{item.title}</h1>
+                 <p className="item__page--description">{item.summary}</p>
+                 <a href={item.url} className="item__page--link">
+                   Link to article
+                 </a>
+               </div>
+             </div>
+           </div>
+         </div>
+          )
+        }
     </>
   );
 }
